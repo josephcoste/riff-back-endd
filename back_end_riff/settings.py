be2e8e9dac6c224281084fce650dc1d55774eaa9
josephcoste <josephcoste0@gmail.com>
2024-05-22 15:55:33 -0400

    update

--- back_end_riff/settings.py
+++ back_end_riff/settings.py
@@ -163,3 +163,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
